b6242da194ec51dc5e22f6c3e35134a5b83d07e0
Andresfetobon <andrestromp777@gmail.com>
2024-03-21 23:03:09 -0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Pokedex from './pages/Pokedex';
 import './App.css';
+import ProtectedRoutes from './pages/ProtectedRoutes';
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route element={<ProtectedRoutes />}>
+          <Route path='/pokedex' element={<Pokedex />} />
+        </Route>
       </Routes>
+      <footer className='pokedex-footer'>
+        <div className='red-section'></div>
+        <div className='black-section'></div>
+      </footer>
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,36 @@
+import { useRef } from 'react';
 import imagen from '../../img/pokedex-3d-logo.png';
-import  '../../Styles/HomeStyles.css'
+import { setTrainerNameG } from '../../store/slices/trainerName.slice';
+import '../../Styles/HomeStyles.css';
+import { useDispatch, useSelector } from 'react-redux';
 
 const Home = () => {
+  const inputNameRef = useRef();
+  // const { trainerName } = useSelector(states => states); 
+  const trainerName = useSelector(state => state.trainerName);
+
+  const dispatch = useDispatch();
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    dispatch(setTrainerNameG(inputNameRef.current.value.trim()));
+    inputNameRef.current.value = '';
+  };
+
+  console.log(trainerName);
+
   return (
     <div className='container-home'>
-      <img style={{ maxWidth: "50%" }} src={imagen} alt='Logo' />
-      <h2 className="welcome-text">!Hola entrenador¡</h2>
-      <p style={{fontSize: "1.5rem"}}>Para poder comenzar dame tu Nombre</p>
-      <form className='pokedex-form'>
-        <input placeholder='Tu Nombre' type='text' />
+      <img style={{ maxWidth: '50%' }} src={imagen} alt='Logo' />
+      <h2 className='welcome-text'>!Hola entrenador¡</h2>
+      <p style={{ fontSize: '1.5rem' }}>Para poder comenzar dame tu Nombre</p>
+      <form className='pokedex-form' onSubmit={handleSubmit}>
+        <input
+          placeholder='Tu Nombre'
+          type='text'
+          ref={inputNameRef}
+          required
+        ></input>
         <button>Comenzar</button>
       </form>
     </div>
